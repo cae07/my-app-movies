296d5eafc4c8f70faf046ad4f75740ff3226dde3
Cae <eduardokae01@hotmail.com>
2021-10-18 21:25:09 -0300

    cria pasta Pages e cria MainPage

---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Cards from './Components/Cards';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+
+function MainPage() {
+  return (
+    <>
+      <Header />
+      <Cards />
+      <Footer />
+    </>
+  );
+}
+
+export default MainPage;
